e0dab46b84fce47492d479efced9ce28bb6f9d68
danibog <dannybg090909@gmail.com>
2023-06-26 16:44:26 +0300

    navigation is finished

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import Header from "./component/Layout/Header";
 import AvailableMeals from "./component/AvailableMeals/Meals";
 import Orders from "./component/Orders/Orders";
+import { Route, Routes } from "react-router-dom";
+
 function App() {
   return (
     <>
       <Header />
       <main>
-        {/* <Orders /> */}
-        <Orders served={true} />
-
-        {/* <AvailableMeals /> */}
+        <Routes>
+          <Route path="/" element={<Orders />}></Route>
+          <Route path="/served" element={<Orders served={true} />}></Route>
+          <Route path="/availableMeals" element={<AvailableMeals />}></Route>
+        </Routes>
       </main>
     </>
   );
--- src/component/Layout/Header.js
+++ src/component/Layout/Header.js
@@ -1,14 +1,42 @@
 import { Fragment } from "react";
 import styles from "./Header.module.css";
 import mealsImg from "../../assets/meals.jpg";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
     <Fragment>
       <header className={styles.header}>
         <h1>Kibinesh Admin</h1>
+        <nav className={styles.nav}>
+          <li>
+            <NavLink
+              to="/"
+              className={({ isActive }) => (isActive ? styles.active : "")}
+            >
+              All Orders
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/served"
+              className={({ isActive }) => (isActive ? styles.active : "")}
+            >
+              Served Orders
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/availableMeals"
+              className={({ isActive }) => (isActive ? styles.active : "")}
+            >
+              Available Meals
+            </NavLink>
+          </li>
+        </nav>
       </header>
       {/* navigation to see available meals , current orders , served orders , finished orders waiting customer, kibinesh kitchn for cooked meals */}
+
       <div className={styles["main-image"]}>
         <img src={mealsImg} alt="delicious food!" />
       </div>
